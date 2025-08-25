502a873df5cd6d22b408d76b40c4ed3a3b78030c
Muhammad Aprilianto <aprilianto665@gmail.com>
2025-08-25 23:58:42 +0700

    creating checkprofile and profile endpoint

--- internal/routes/profile_routes.go
+++ internal/routes/profile_routes.go
@@ -1,2 +1,11 @@
 package routes
 
+import "github.com/gofiber/fiber/v2"
+
+func ProfileRoutes(app *fiber.App){
+	profile := app.Group("profile")
+
+	profile.Get("/checkprofile/:uid", func (ctx *fiber.Ctx) error {})
+	profile.Get("/profile/:uid", func (ctx *fiber.Ctx) error {})
+
+}
