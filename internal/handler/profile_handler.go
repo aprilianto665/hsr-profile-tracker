020e37fc135d092eb73e910ae377d7b1ba8c48e0
Muhammad Aprilianto <aprilianto665@gmail.com>
2025-08-26 00:48:14 +0700

    integrations mihomo api to getprofile handler

--- internal/handler/profile_handler.go
+++ internal/handler/profile_handler.go
@@ -1,11 +1,50 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func CheckProfile(ctx *fiber.Ctx) error {
 	return ctx.SendString("Check Profile")
 }
 
 func GetProfile(ctx *fiber.Ctx) error {
-	return ctx.SendString("Get Profile")
+	uid := ctx.Params("uid")
+
+	if uid == "" {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status": "error",
+			"message": "uid is required",
+		})
+	}
+
+	url := fmt.Sprintf("https://api.mihomo.me/sr_info_parsed/%s?lang=en",uid)
+
+	agent := fiber.Get(url).UserAgent("hsr-profile-tracker/1.0").Timeout(10 * time.Second)
+
+	statusCode, body, errs := agent.Bytes()
+	if len(errs) > 0 {
+		return ctx.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"status": "error",
+			"message": "failed to fetch from Mihomo",
+		})
+	}
+
+	if statusCode < 200 || statusCode >= 300 {
+		return ctx.Status(statusCode).Send(body)
+	}
+
+	var data interface{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status": "error",
+			"message": "failed to parse response",
+		})
+	}
+
+	return ctx.Status(statusCode).JSON(data)
 }
