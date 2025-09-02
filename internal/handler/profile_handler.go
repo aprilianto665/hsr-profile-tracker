6b5fa274d5bd2171969d6a7f35ec40fc557ebd9c
Muhammad Aprilianto <aprilianto665@gmail.com>
2025-09-02 19:12:24 +0700

    feat: add Redis caching for CheckProfile and GetProfile endpoints

--- internal/handler/profile_handler.go
+++ internal/handler/profile_handler.go
@@ -3,11 +3,13 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"hsr-profile-tracker/internal/database"
 	"hsr-profile-tracker/internal/model"
 	"hsr-profile-tracker/internal/util"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
 )
 
 func CheckProfile(ctx *fiber.Ctx) error {
@@ -18,6 +20,17 @@ func CheckProfile(ctx *fiber.Ctx) error {
 			"status":  "error",
 			"message": "UID is required",
 		})
+	}
+
+	if database.Rdb != nil {
+		if cachedBytes, err := database.Rdb.Get(database.Ctx, uid).Bytes(); err == nil && len(cachedBytes) > 0 {
+			return ctx.Status(fiber.StatusOK).JSON(model.CheckProfileResponse{
+				Status:  "success",
+				Message: "Profile exists",
+				Exists:  true,
+			})
+		} else if err != nil && err != redis.Nil {
+		}
 	}
 
 	url := fmt.Sprintf("https://api.mihomo.me/sr_info_parsed/%s?lang=en", uid)
@@ -56,6 +69,20 @@ func GetProfile(ctx *fiber.Ctx) error {
 			"status":  "error",
 			"message": "UID is required",
 		})
+	}
+
+	if database.Rdb != nil {
+		if cachedBytes, err := database.Rdb.Get(database.Ctx, uid).Bytes(); err == nil && len(cachedBytes) > 0 {
+			var cachedSummary model.ProfileSummary
+			if err := json.Unmarshal(cachedBytes, &cachedSummary); err == nil {
+				return ctx.Status(fiber.StatusOK).JSON(model.APIProfileResponse{
+					Status:  "success",
+					Message: "Profile fetched from cache",
+					Data:    cachedSummary,
+				})
+			}
+		} else if err != nil && err != redis.Nil {
+		}
 	}
 
 	url := fmt.Sprintf("https://api.mihomo.me/sr_info_parsed/%s?lang=en", uid)
@@ -118,12 +145,18 @@ func GetProfile(ctx *fiber.Ctx) error {
 		})
 	}
 
+	summary := model.ProfileSummary{
+		Player:     player,
+		Characters: chars,
+	}
+
+	if b, err := json.Marshal(summary); err == nil {
+		database.Rdb.Set(database.Ctx, uid, b, time.Hour)
+	}
+
 	return ctx.Status(statusCode).JSON(model.APIProfileResponse{
 		Status:  "success",
 		Message: "Profile fetched successfully",
-		Data: model.ProfileSummary{
-			Player:     player,
-			Characters: chars,
-		},
+		Data:    summary,
 	})
 }
