5af9c787be3d9d9c036386fb636cf28613fdeb23
Muhammad Aprilianto <aprilianto665@gmail.com>
2025-09-04 18:32:56 +0700

    feat(stats): add FindEffectiveStats function

--- internal/util/relic_score.go
+++ internal/util/relic_score.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"hsr-profile-tracker/internal/configs"
 	"hsr-profile-tracker/internal/model"
+	"reflect"
 )
 
 func FindCharacterWeights(player model.Player, char model.Character) model.CharacterWeights {
@@ -20,7 +21,24 @@ func FindCharacterWeights(player model.Player, char model.Character) model.Chara
 	return result
 }
 
+func FindEffectiveStats(statType string) float64 {
+	value := reflect.ValueOf(*configs.EffectiveStats)
+	field := value.FieldByName(statType).Float()
+
+	return field
+}
+
 func CalculateRelicScore(player model.Player, char model.Character) {
-	data := FindCharacterWeights(player, char)
-	fmt.Println(data.Character)
+	charWeight := FindCharacterWeights(player, char)
+
+	for _, r := range char.Relics {
+		fmt.Println(r.Name)
+		eff := FindEffectiveStats(r.MainAffix.Type)
+		fmt.Println(r.MainAffix.Name, eff)
+
+		for _, sub := range r.SubAffix {
+			charStatWeight := charWeight.SubstatWeights[sub.Type]
+			fmt.Println(sub.Type, charStatWeight)
+		}
+	}
 }
